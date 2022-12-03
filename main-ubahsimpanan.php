d7d0fe799d4d6dad9c1b8019242da1becda1f22d
Rizqi Nur Andi Putra <92218072+Thecaracter@users.noreply.github.com>
2022-12-04 01:09:53 +0700

    penambahan popup

--- main-ubahsimpanan.php
+++ main-ubahsimpanan.php
@@ -1,6 +1,9 @@
 <?php include 'koneksi.php';
 include 'funct.php';
 ?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 <div class="content-wrapper">
     <section class="content-header">
         <h1>
@@ -11,6 +14,7 @@ include 'funct.php';
             <li><a href="index.php"><i class="fa fa-dashboard"></i> Master</a></li>
             <li class="active">Simpanan</li>
             <li class="active">Daftar Simpanan</li>
+
         </ol>
     </section>
     <section class="content">
@@ -24,6 +28,9 @@ include 'funct.php';
                 $sql = mysqli_query($conn, "SELECT * FROM k_simpanan");
                 ?>
                 <table id="dataTable" class="table table-bordered table-striped">
+                    <button type="button" name="age" id="age" data-toggle="modal" data-target="#add_data_Modal"
+                        class="btn btn-warning">Tambah Data Karyawan</button>
+                    <br></br>
                     <thead>
                         <tr class="info">
                             <th>
@@ -67,19 +74,18 @@ include 'funct.php';
                             </td>
                             <td>
                                 <center>
+
                                     <a href="page-form-anggota.php?id_anggota=<?php echo $data['id_anggota']; ?>&reqa=edit"
                                         title="Edit Data ini" class="btn btn-danger btn-sm"><i class="fa fa-edit "></i>
                                         Edit
                                     </a>
-                                    <a href="proses.php?id_anggota=<?php echo $data['id_anggota']; ?>&reqa=dell"
-                                        title="Berhenti Jadi Anggota" class="btn btn-success btn-sm"><span
-                                            class="fa fa-info"> Detail</span>
-                                    </a>
-                                    <a href="proses.php?id_anggota=<?php echo $data['id_anggota']; ?>&reqa=dell"
-                                        title="Berhenti Jadi Anggota" class="btn btn-warning btn-sm"
-                                        onClick="return confirm('Yakin mau berhenti?');"><span class="fa fa-share">
+
+                                    <a href="proses.php?id=<?php echo $data['id']; ?>&reqaus=dell"
+                                        title="Hapus Simpanan" class="btn btn-warning btn-sm"
+                                        onClick="return confirm('Yakin mau berhenti?');"><span class="fa fa-trash">
                                             Berhenti</span>
                                     </a>
+
                                 </center>
                             </td>
                         </tr>
@@ -87,6 +93,7 @@ include 'funct.php';
                                 $no++;
                             }
                             ?>
+
                     </tbody>
                 </table>
                 <!-- /.box -->
@@ -97,3 +104,129 @@ include 'funct.php';
     </section>
     <!-- /.content -->
 </div>
+
+</html>
+
+<div id="add_data_Modal" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Input Data Dengan Menggunakan Modal Bootstrap</h4>
+            </div>
+            <div class="modal-body">
+                <form method="post" id="insert_form">
+                    <label>Nama Simpanan</label>
+                    <input type="text" name="nama" id="nama" class="form-control" />
+                    <br />
+                    <label>Keterangan simpanan</label>
+                    <textarea name="alamat" id="alamat" class="form-control"></textarea>
+                    <br />
+                    <label>Besar Simpanan</label>
+                    <input type="text" name="umur" id="umur" class="form-control" />
+                    <br />
+                    <input type="submit" name="insert" id="insert" value="Insert" class="btn btn-success" />
+
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="dataModal" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Detail Data Karyawan</h4>
+            </div>
+            <div class="modal-body" id="detail_karyawan">
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div id="editModal" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Edit Data Karyawan</h4>
+            </div>
+            <div class="modal-body" id="form_edit">
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    $(document).ready(function () {
+        // Begin Aksi Insert
+        $('#insert_form').on("submit", function (event) {
+            event.preventDefault();
+            // if ($('#nama').val() == "") {
+            //     alert("Mohon Isi Nama ");
+        }
+            // else if ($('#alamat').val() == '') {
+            //     alert("Mohon Isi Alamat");
+            }
+
+            else {
+            $.ajax({
+                url: "insert.php",
+                method: "POST",
+                data: $('#insert_form').serialize(),
+                beforeSend: function () {
+                    $('#insert').val("Inserting");
+                },
+                success: function (data) {
+                    $('#insert_form')[0].reset();
+                    $('#add_data_Modal').modal('hide');
+                    $('#employee_table').html(data);
+                }
+            });
+        }
+        });
+    //END Aksi Insert
+    //Begin Tampil Form Edit
+    $(document).on('click', '.edit_data', function () {
+        var employee_id = $(this).attr("id");
+        $.ajax({
+            url: "edit.php",
+            method: "POST",
+            data: { employee_id: employee_id },
+            success: function (data) {
+                $('#form_edit').html(data);
+                $('#editModal').modal('show');
+            }
+        });
+    });
+    //End Tampil Form Edit
+
+    //Begin Aksi Delete Data
+    $(document).on('click', '.hapus_data', function () {
+        var employee_id = $(this).attr("id");
+        $.ajax({
+            url: "delete.php",
+            method: "POST",
+            data: { employee_id: employee_id },
+            success: function (data) {
+                $('#employee_table').html(data);
+            }
+        });
+    });
+    });
+//End Aksi Delete Data
+</script>
