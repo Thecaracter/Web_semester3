872459ebfd789846862cab5c5bc1e84a025ea4dc
nabiladafa <cahyakirana25@gmail.com>
2022-12-25 13:01:10 +0700

    web page icon

--- page-cetak-simpanan.php
+++ page-cetak-simpanan.php
@@ -6,6 +6,8 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Koperasi | Form Simpanan</title>
+  <link href="img/logo_koperasi.png" rel="icon">
+  <link href="img/logo_koperasi.png" rel="apple-touch-icon">
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
--- page-simpanan.php
+++ page-simpanan.php
@@ -4,6 +4,8 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Koperasi | Page Simpanan</title>
+  <link href="img/logo_koperasi.png" rel="icon">
+  <link href="img/logo_koperasi.png" rel="apple-touch-icon">
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
